f4ffffe80449b09502538ce23e9f7be91f6f3907
michaeljamesrojas <michaeljamesrojas123@gmail.com>
2025-07-18 06:25:37 +0800

    fix scanner turning black when navigating to and from the scanner

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -34,6 +34,13 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
+        name="scanner"
+        options={{
+          title: 'Scanner',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="qrcode" color={color} />,
+        }}
+      />
+      <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
